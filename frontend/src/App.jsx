b37f18532d533bdb6a498ddbf88ab7bb91eeb0d1
Fahad Kamal <fahadkamal@hotmail.com>
2024-04-15 10:43:36 -0400

    create todo state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import TodoList from "./TodoList"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState({})
 
   return (
     <>
-      <form>
+      <form >
         <label htmlFor="todo">Enter Todo:</label>
         <input type="text" name="todoitem" id="todo"></input>
         <input type="submit" value="Submit"></input>
-      </form>
-      <TodoList />
+      </form >
+      
+      <TodoList todos={todos}/>
       
     </>
   )
