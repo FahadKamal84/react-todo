5745f39a9cc98edc6efc3b68449dd69e226dc7f0
Fahad Kamal <fahadkamal@hotmail.com>
2024-04-16 11:01:03 -0400

    declare handleChange function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,14 @@ import TodoList from "./TodoList"
 function App() {
   const [todos, setTodos] = useState({})
 
+  const handleChange = (event) => {
+    setTodos(event.target.name)
+  }
   return (
     <>
       <form >
         <label htmlFor="todo">Enter Todo:</label>
-        <input type="text" name="todoitem" id="todo"></input>
+        <input type="text" name="todoitem" id="todo" onChange={handleChange}></input>
         <button type="submit">Submit</button>
       </form >
       
